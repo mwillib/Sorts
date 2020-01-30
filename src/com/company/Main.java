5f3b915895d4eb3292b02b23e1934154d3bd8dd1
mwillib <mike.williamsbackshall@aexp.com>
2020-01-30 13:35:00 +0000

    User can set Array Length in Console

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,29 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        int arrayLength = 7;
+        //Creating a Scanner to set the Array's Length
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Set Array Length:");
+        int arrayLength = scanner.nextInt();
         int[] array = new int[arrayLength];
+
+        //Creating a New Random Array for each sort.
         for(int i = 0; i < arrayLength; i++){
             array[i] = (int) (Math.random()*10);
         }
-
         Selection.main(array);
 
+        //Creating a New Random Array for each sort.
         for(int i = 0; i < arrayLength; i++){
             array[i] = (int) (Math.random()*10);
         }
-
         Insertion.main(array);
-
+//        //Creating a New Random Array for each sort.
 //        for(int i = 0; i < arrayLength; i++){
 //            array[i] = (int) (Math.random()*10);
 //        }
